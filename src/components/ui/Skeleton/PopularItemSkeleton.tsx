f871feea3227169f77387200a05e27343f63b6b1
htak0601 <taklee2@naver.com>
2026-07-26 16:36:17 +0900

    fix: 인기 체험 skeleton 개선

--- src/components/ui/Skeleton/PopularItemSkeleton.tsx
+++ src/components/ui/Skeleton/PopularItemSkeleton.tsx
@@ -1,8 +1,8 @@
 const PopularItemSkeleton = () => {
   return (
-    <li className='w-full max-w-none animate-pulse lg:max-w-[390px]'>
-      <div className='relative aspect-square w-full rounded-[20px] bg-gray-200'></div>
-    </li>
+    <div className='w-full max-w-none animate-pulse lg:max-w-[390px]'>
+      <div className='relative aspect-square w-full rounded-[20px] bg-gray-200' />
+    </div>
   );
 };
 
