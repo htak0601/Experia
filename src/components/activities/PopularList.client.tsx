8a2634cf92b2a1bd72c561e99ae63d53f517b4de
htak0601 <taklee2@naver.com>
2026-07-26 16:31:46 +0900

    fix: 인기 체험 Swiper 초기 렌더링 문제 개선

--- src/components/activities/PopularList.client.tsx
+++ src/components/activities/PopularList.client.tsx
@@ -1,3 +1,66 @@
+// 'use client';
+
+// import LoadingSpinner from '@/components/ui/LoadingSpinner';
+// import { BREAKPOINTS, POPULAR_ACTIVITIES_VIEW_COUNT } from '@/constants';
+// import { PopularActivities } from '@/types/schema/activitiesSchema';
+// import { InfiniteData } from '@tanstack/react-query';
+// import { Navigation } from 'swiper/modules';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import PopularItem from './PopularItem';
+
+// interface PopularListProps {
+//   data: InfiniteData<PopularActivities, string | null>;
+//   fetchNextPage: () => void;
+//   hasNextPage: boolean;
+//   isFetchingNextPage: boolean;
+// }
+
+// const PopularList = ({
+//   data,
+//   fetchNextPage,
+//   hasNextPage,
+//   isFetchingNextPage,
+// }: PopularListProps) => {
+//   const isFetchingMore = hasNextPage && isFetchingNextPage;
+//   return (
+//     <div className='relative w-full overflow-hidden'>
+//       <Swiper
+//         className='w-full'
+//         slidesPerView={POPULAR_ACTIVITIES_VIEW_COUNT.sm} // 모바일 기본
+//         observer
+//         observeParents
+//         resizeObserver
+//         spaceBetween={16} // 슬라이드 간격
+//         breakpoints={{
+//           [BREAKPOINTS.md]: { slidesPerView: POPULAR_ACTIVITIES_VIEW_COUNT.md }, // 720px 이상
+//           [BREAKPOINTS.lg]: { slidesPerView: POPULAR_ACTIVITIES_VIEW_COUNT.lg }, // 1280px 이상
+//         }}
+//         navigation // 좌우 버튼
+//         grabCursor // 마우스 드래그 가능
+//         modules={[Navigation]} // Swiper 모듈 등록
+//         onReachEnd={() => {
+//           if (hasNextPage) fetchNextPage();
+//         }}
+//       >
+//         {data.pages.map(page =>
+//           page.activities.map((activity, i) => (
+//             <SwiperSlide key={activity.id} className='!h-auto'>
+//               <PopularItem item={activity} idx={i} />
+//             </SwiperSlide>
+//           )),
+//         )}
+//         {isFetchingMore && (
+//           <SwiperSlide className='flex w-auto items-center justify-center'>
+//             <LoadingSpinner />
+//           </SwiperSlide>
+//         )}
+//       </Swiper>
+//     </div>
+//   );
+// };
+
+// export default PopularList;
+
 'use client';
 
 import LoadingSpinner from '@/components/ui/LoadingSpinner';
@@ -22,32 +85,43 @@ const PopularList = ({
   isFetchingNextPage,
 }: PopularListProps) => {
   const isFetchingMore = hasNextPage && isFetchingNextPage;
+
   return (
     <div className='relative'>
       <Swiper
-        slidesPerView={POPULAR_ACTIVITIES_VIEW_COUNT.sm} // 모바일 기본
-        spaceBetween={16} // 슬라이드 간격
+        className='w-full'
+        slidesPerView={POPULAR_ACTIVITIES_VIEW_COUNT.sm}
+        spaceBetween={16}
         breakpoints={{
-          [BREAKPOINTS.md]: { slidesPerView: POPULAR_ACTIVITIES_VIEW_COUNT.md }, // 720px 이상
-          [BREAKPOINTS.lg]: { slidesPerView: POPULAR_ACTIVITIES_VIEW_COUNT.lg }, // 1280px 이상
+          [BREAKPOINTS.md]: {
+            slidesPerView: POPULAR_ACTIVITIES_VIEW_COUNT.md,
+          },
+          [BREAKPOINTS.lg]: {
+            slidesPerView: POPULAR_ACTIVITIES_VIEW_COUNT.lg,
+          },
         }}
-        navigation // 좌우 버튼
-        grabCursor // 마우스 드래그 가능
-        modules={[Navigation]} // Swiper 모듈 등록
+        navigation
+        grabCursor
+        modules={[Navigation]}
         onReachEnd={() => {
-          if (hasNextPage) fetchNextPage();
+          if (hasNextPage && !isFetchingNextPage) {
+            fetchNextPage();
+          }
         }}
       >
         {data.pages.map(page =>
-          page.activities.map((activity, i) => (
+          page.activities.map(activity => (
             <SwiperSlide key={activity.id}>
-              <PopularItem item={activity} idx={i} />
+              <PopularItem item={activity} />
             </SwiperSlide>
           )),
         )}
+
         {isFetchingMore && (
-          <SwiperSlide className='flex w-auto items-center justify-center'>
-            <LoadingSpinner />
+          <SwiperSlide>
+            <div className='flex aspect-square w-full items-center justify-center'>
+              <LoadingSpinner />
+            </div>
           </SwiperSlide>
         )}
       </Swiper>
